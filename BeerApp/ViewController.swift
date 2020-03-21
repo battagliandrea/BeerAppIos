bb7284c3eae40fae2cec715c575c8ef50eaeaf04
Andrea Battaglia <battaglia.andry@gmail.com>
2020-03-21 20:53:47 +0100

    Initial Commit

---
+++ BeerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerApp
+//
+//  Created by Andrea on 21/03/2020.
+//  Copyright © 2020 Andrea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
